9e6ea76381edfd44e9f46939f4eaf5dc5cf853fa
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-23 01:12:19 +0300

    Update config.py
    Added secret key

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wainainakasyoka:W41n41n4@localhost/pitchme'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
     @staticmethod
     def init_app(app):
