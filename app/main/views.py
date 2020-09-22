18c464615f17fa98950b4b0dad227d5953c1bd3e
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-22 20:25:59 +0300

    Added update route

---
+++ app/main/views.py
@@ -0,0 +1,36 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from . import main
+from .forms import UpdateProfile
+from flask_login import login_required, current_user
+from .. import db
+from ..models import User,Role 
+
+
+
+@main.route('/')
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    title = 'Home - Welcome to The Pitch of Your Life'
+
+    return render_template('index.html', title=title)
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
