24c582b3ea7cfa3fc46d3bf758a28af4273b2a60
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-23 02:27:50 +0300

    Update config "setup SMTP server"

--- config.py
+++ config.py
@@ -7,6 +7,16 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'nainacreatives@gmail.com'
+
+
     @staticmethod
     def init_app(app):
         pass
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+export MAIL_USERNAME=nainacreatives@gmail.com
+export MAIL_PASSWORD=shak3m3up
 export SECRET_KEY='pomodoro'
 python3.8 manage.py server
