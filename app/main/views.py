a9903de630245dc8074d8f013111e5fc1f77e3ae
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-22 21:28:54 +0300

    Update main views.py
    create profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -34,3 +34,12 @@ def update_profile(uname):
         return redirect(url_for('.profile', uname=user.username))
 
     return render_template('profile/update.html', form=form)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
