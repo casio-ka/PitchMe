0c6e25b9702022da5f06948ae453d8dc67fafcb0
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-24 21:39:17 +0300

    created form classes
    Promotion pitch form
    pickuplines form
    product pitch
    interviewpitch
    promotion comment

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,27 @@ from ..models import User
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
+
+class PromotionPitch(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class PickupLines(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class ProductPitch(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class InterviewPitch(FlaskForm):
+    post = StringField('Title', validators=[Required()])
+    body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class PromotionComment(FlaskForm):
+    comment = StringField('Comment: ', validators=[Required()])
+    submit = SubmitField('Submit')
