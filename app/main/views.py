cf8afeff26e1eaefcab85539124f0c9e8c797ba8
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-27 19:58:52 +0300

    Update views.py
    Added new routes for comments and likes

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from . import main
-from .forms import UpdateProfile,PromotionPitch, PickupLines, ProductPitch,InterviewPitch,PromotionComment
+from .forms import UpdateProfile,PromotionPitch, PickupLinesPitch, ProductPitch,InterviewPitch,PromotionComment,ProductComment,PickupComment,InterviewComment
 from flask_login import login_required, current_user
 from .. import db,photos
-from ..models import User,Role, Promotion,PickupLines,Product,Interview,PromotionComments
+from ..models import User,Role, Promotion,PickupLines,Product,Interview,PromotionComments,ProductComments,PickupComments,InterviewComments
 
 
 
@@ -72,18 +72,18 @@ def promotion():
     return render_template("promotionpitch.html", promotion_form=form, title=title)
 
 
-@main.route('/category/pickuplines', methods=['GET', 'POST'])
+@main.route('/category/pickuppitch', methods=['GET', 'POST'])
 @login_required
-def pickuplines():
-    form = PickupLines()
+def pickuppitch():
+    form = PickupLinesPitch()
     title = 'Post a pitch'
     if form.validate_on_submit():
         post = form.post.data
         body = form.body.data
-        new_pick = PickupLines(post=post, user=current_user, body=body)
-        new_pick.save_pick()
-        return redirect(url_for('.listpickup'))
-    return render_template("pickuppitch.html", pickup_form=form, title=title)
+        new_pickupline = PickupLines(post=post, user=current_user, body=body)
+        new_pickupline.save_pickuplines()
+        return redirect(url_for('.listpickuppitch'))
+    return render_template("pickup_pitch.html", pickup_form=form, title=title)
 
 @main.route('/category/productpitch', methods=['GET', 'POST'])
 @login_required
@@ -95,7 +95,7 @@ def productpitch():
         body = form.body.data
         new_product = Product(post=post, user=current_user, body=body)
         new_product.save_product()
-        return redirect(url_for('.listproduct'))
+        return redirect(url_for('.listproducts'))
     return render_template("productpitch.html", product_form=form)
 
 @main.route('/category/interviewpitch', methods=['GET', 'POST'])
@@ -107,25 +107,138 @@ def interview():
         post = form.post.data
         body = form.body.data
         new_interview = Interview(post=post, user=current_user, body=body)
-        new_interview.save_production()
+        new_interview.save_interview()
         return redirect(url_for('.listinterview'))
     return render_template("interviewpitch.html", interview_form=form,title=title)
 
-@main.route('/category/promotion')
+#Productlist
+
+@main.route('/category/product')
 def listproducts():
+    title = 'Products'
+    posts = Product.query.all()
+    return render_template("products.html", mypost=posts,title=title)
+
+
+@main.route('/product/<int:id>', methods=['POST', 'GET'])
+def displayproduct(id):
+    products = Product.query.get(id)
+    form = ProductComment()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_productcomment = ProductComments(comment=comment, product_id=id, user=current_user)
+        new_productcomment.save_productcomments()
+
+    comments = ProductComments.query.filter_by(product_id=id).all()
+    return render_template('prod_display.html', products=products, comment_form=form, comments=comments)
+
+#Promolist
+    
+@main.route('/category/promotion')
+def listpromotion():
     title = 'Promotion'
     posts = Promotion.query.all()
     return render_template("promotion.html", mypost=posts,title=title)
 
 
 @main.route('/promotion/<int:id>', methods=['POST', 'GET'])
-def displayproduct(id):
-    promotion = Promotion.query.get(id)
+def displaypromotion(id):
+    promo = Promotion.query.get(id)
     form = PromotionComment()
     if form.validate_on_submit():
         comment = form.comment.data
-        new_promo_comment = PromotionComments(comment=comment, promotion_id=id, user=current_user)
-        new_promo_comment.save_promo_coments()
+        new_promotioncomment = PromotionComments(comment=comment, promo_id=id, user=current_user)
+        new_promotioncomment.save_promotioncomments()
 
-    comments = PromotionComments.query.filter_by(promotion_id=id).all()
-    return render_template('promotionpitch.html', promotion=promotion, comment_form=form, comments=comments)
+    comments = ProductComments.query.filter_by(promo_id=id).all()
+    return render_template('promo_display.html', promo=promo, comment_form=form, comments=comments)
+
+ #PickupList
+
+@main.route('/category/pickuplines')
+def listpickuppitch():
+    title = 'Pickup Lines'
+    posts = PickupLines.query.all()
+    return render_template("pickup.html", mypost=posts,title=title)
+
+
+@main.route('/pickuplines/<int:id>', methods=['POST', 'GET'])
+def displaypickupline(id):
+    pickup = PickupLines.query.get(id)
+    form = PickupComment()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_pickupcomment = PickupComments(comment=comment, pickuplines_id=id, user=current_user)
+        new_pickupcomment.save_pickupcomments()
+
+    comments = PickupComments.query.filter_by(pickuplines_id=id).all()
+    return render_template('pickup_display.html', pickup=pickup, comment_form=form, comments=comments)
+
+#InterviewList
+
+@main.route('/category/interview')
+def listinterview():
+    title = 'Interviews'
+    posts = Interview.query.all()
+    return render_template("interview.html", mypost=posts,title=title)
+
+
+@main.route('/interview/<int:id>', methods=['POST', 'GET'])
+def displayinterview(id):
+    interview = Interview.query.get(id)
+    form = InterviewComment()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_interviewcomment = InterviewComments(comment=comment, interview_id=id, user=current_user)
+        new_interviewcomment.save_interviewcomments()
+
+    comments = InterviewComments.query.filter_by(interview_id=id).all()
+    return render_template('interview_display.html', interview=interview, comment_form=form, comments=comments)
+
+@main.route('/like/<int:interview_id>/<action>')
+@login_required
+def interviewlike_action(interview_id, action):
+    interviewlikes = Interview.query.filter_by(id=interview_id).first_or_404()
+    if action == 'like':
+        current_user.like_post(interviewlikes)
+        db.session.commit()
+    if action == 'unlike':
+        current_user.unlike_post(interviewlikes)
+        db.session.commit()
+    return redirect(request.referrer)
+
+@main.route('/like/<int:product_id>/<action>')
+@login_required
+def productlike_action(product_id, action):
+    productlikes = Product.query.filter_by(id=product_id).first_or_404()
+    if action == 'like':
+        current_user.like_post(productlikes)
+        db.session.commit()
+    if action == 'unlike':
+        current_user.unlike_post(productlikes)
+        db.session.commit()
+    return redirect(request.referrer)
+
+@main.route('/like/<int:pickuplines_id>/<action>')
+@login_required
+def pickuplike_action(pickuplines_id, action):
+    pickupikes = PickupLines.query.filter_by(id=pickuplines_id).first_or_404()
+    if action == 'like':
+        current_user.like_post(pickupikes)
+        db.session.commit()
+    if action == 'unlike':
+        current_user.unlike_post(pickupikes)
+        db.session.commit()
+    return redirect(request.referrer)
+
+@main.route('/like/<int:promotion_id>/<action>')
+@login_required
+def promolike_action(promotion_id, action):
+    promolikes = Promotion.query.filter_by(id=promotion_id).first_or_404()
+    if action == 'like':
+        current_user.like_post(promolikes)
+        db.session.commit()
+    if action == 'unlike':
+        current_user.unlike_post(promolikes)
+        db.session.commit()
+    return redirect(request.referrer)
