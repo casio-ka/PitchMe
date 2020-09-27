6064680c8510b46a77071e021cbabf4cd7fe1c7b
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-09-27 19:57:04 +0300

    Update views.py
    changed the arrangement of code

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,26 +7,6 @@ from .. import db
 from ..email import mail_message
 
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
-
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("main.index"))
-
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
@@ -38,5 +18,29 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "PitchMe login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
+
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message(" - Welcome to PitchMe","email/welcome_user", user.email, user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
